7c0bebe644ebda95a706267469077d520a795d62
Paul Ambrose <pambrose@mac.com>
2009-09-10 11:46:29 -0700

    Misc cleanup

--- src/main/java/com/imap4j/hbase/hbql/expr/value/func/GenericTernary.java
+++ src/main/java/com/imap4j/hbase/hbql/expr/value/func/GenericTernary.java
@@ -4,6 +4,7 @@ import com.imap4j.hbase.hbase.HPersistException;
 import com.imap4j.hbase.hbql.expr.ExprVariable;
 import com.imap4j.hbase.hbql.expr.node.PredicateExpr;
 import com.imap4j.hbase.hbql.expr.node.ValueExpr;
+import com.imap4j.hbase.hbql.expr.value.GenericTwoExprExpr;
 import com.imap4j.hbase.hbql.schema.ExprSchema;
 
 import java.util.List;
@@ -14,39 +15,21 @@ import java.util.List;
  * Date: Aug 31, 2009
  * Time: 1:51:03 PM
  */
-public abstract class GenericTernary<T extends ValueExpr> {
+public abstract class GenericTernary<T extends ValueExpr> extends GenericTwoExprExpr<T> {
 
     private PredicateExpr pred = null;
-    private T expr1 = null, expr2 = null;
 
-    public GenericTernary(final PredicateExpr pred, final T expr1, final T expr2) {
+    protected GenericTernary(final PredicateExpr pred, final T expr1, final T expr2) {
+        super(expr1, expr2);
         this.pred = pred;
-        this.expr1 = expr1;
-        this.expr2 = expr2;
     }
 
     protected PredicateExpr getPred() {
         return this.pred;
     }
 
-    protected T getExpr1() {
-        return this.expr1;
-    }
-
-    protected T getExpr2() {
-        return this.expr2;
-    }
-
     protected void setPred(final PredicateExpr pred) {
         this.pred = pred;
-    }
-
-    protected void setExpr1(final T expr1) {
-        this.expr1 = expr1;
-    }
-
-    protected void setExpr2(final T expr2) {
-        this.expr2 = expr2;
     }
 
     public List<ExprVariable> getExprVariables() {
@@ -57,7 +40,6 @@ public abstract class GenericTernary<T extends ValueExpr> {
     }
 
     public Object getValue(final Object object) throws HPersistException {
-
         if (this.getPred().evaluate(object))
             return this.getExpr1().getValue(object);
         else
