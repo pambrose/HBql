7d6609b4827e4a751a3f5553f07ab583e53a2498
Paul Ambrose <pambrose@mac.com>
2009-09-29 15:53:12 -0700

    Misc cleanup

--- src/main/java/org/apache/hadoop/hbase/hbql/query/expr/ExprTree.java
+++ src/main/java/org/apache/hadoop/hbase/hbql/query/expr/ExprTree.java
@@ -45,9 +45,6 @@ public class ExprTree extends ExprContext implements Serializable {
 
     public void setSchema(final Schema schema, final List<String> fieldList) throws HBqlException {
 
-        if (schema == null)
-            return;
-
         if (this.isValid()) {
 
             this.setSchema(schema);
