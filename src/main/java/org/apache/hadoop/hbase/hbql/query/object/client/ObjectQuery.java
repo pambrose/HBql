e3ee49ab399af12640879b1ad735aaf2c9765c4c
Paul Ambrose <pambrose@mac.com>
2009-10-07 08:52:00 -0700

    Misc cleanup

--- src/main/java/org/apache/hadoop/hbase/hbql/query/object/client/ObjectQuery.java
+++ src/main/java/org/apache/hadoop/hbase/hbql/query/object/client/ObjectQuery.java
@@ -5,11 +5,12 @@ import org.apache.hadoop.hbase.hbql.client.HBqlException;
 import java.util.Collection;
 
 public interface ObjectQuery<T> {
-    void addListener(ObjectQueryListener<T> listener);
-
-    void clearListeners();
 
     String getQuery();
 
     ObjectResults<T> execute(Collection<T> objs) throws HBqlException;
+
+    void addListener(ObjectQueryListener<T> listener);
+
+    void clearListeners();
 }
