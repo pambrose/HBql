0b979862afab22fb0607da7555901fd7ba68664c
Paul Ambrose <pambrose@mac.com>
2009-10-22 22:26:58 -0700

    Misc cleanup

--- src/test/java/org/apache/hadoop/hbase/hbql/ExamplesTest.java
+++ src/test/java/org/apache/hadoop/hbase/hbql/ExamplesTest.java
@@ -57,12 +57,33 @@ public class ExamplesTest extends TestSupport {
     public void defineSchema() throws HBqlException, IOException {
 
         // START SNIPPET: define-schema1
+        // Schema named foo that corresponds to table foo.
         SchemaManager.execute("DEFINE SCHEMA foo (keyval key, family1:val1 STRING)");
         // END SNIPPET: define-schema1
 
         // START SNIPPET: define-schema2
+        // Schema named schema1 that corresponds to table foo.
         SchemaManager.execute("DEFINE SCHEMA schema1 FOR TABLE foo (keyval key, family1:val1 STRING ALIAS val2)");
         // END SNIPPET: define-schema2
+
+        // START SNIPPET: define-schema3
+        // A column with a default value.
+        SchemaManager.execute("DEFINE SCHEMA schema1 FOR TABLE foo "
+                              + "("
+                              + "keyval key, "
+                              + "family1:val1 STRING ALIAS val2 DEFAULT 'this is a default value'"
+                              + ")");
+        // END SNIPPET: define-schema3
+
+        // START SNIPPET: define-schema4
+        // A schema with a family default attribute.
+        SchemaManager.execute("DEFINE SCHEMA schema1 FOR TABLE foo "
+                              + "("
+                              + "keyval key, "
+                              + "family1:val1 STRING ALIAS val2, "
+                              + "family1:* ALIAS family1_default"
+                              + ")");
+        // END SNIPPET: define-schema4
 
     }
 }
