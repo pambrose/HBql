ab1ef910632d366f499d0347349f8c208c31e2f5
Paul Ambrose <pambrose@mac.com>
2009-10-23 17:14:03 -0700

    Misc cleanup

--- src/main/java/org/apache/hadoop/hbase/hbql/client/HFamily.java
+++ src/main/java/org/apache/hadoop/hbase/hbql/client/HFamily.java
@@ -1,9 +1,9 @@
+// START SNIPPET: doc
 package org.apache.hadoop.hbase.hbql.client;
 
 @java.lang.annotation.Target({java.lang.annotation.ElementType.TYPE})
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 
-// START SNIPPET: doc
 public @interface HFamily {
 
     public abstract String name() default "";
