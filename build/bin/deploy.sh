0357682a9d6803de2d2184e51e2e9d2d5fc03edf
Paul Ambrose <pambrose@mac.com>
2009-11-05 14:50:34 -0800

    Cobbled together a distribution after giving up on maven packaging.

---
+++ build/bin/deploy.sh
@@ -0,0 +1,19 @@
+
+cp -r ./lib ./target/hbql-0.9.0-alpha
+cp -r ./bin ./target/hbql-0.9.0-alpha
+cp -r ./target/site ./target/hbql-0.9.0-alpha
+
+cd target/classes
+jar cf ../../target/hbql-0.9.0-alpha/hbql-0.9.0-alpha.jar *
+cd ../..
+
+mkdir ./target/site/downloads
+cd target
+zip -r ./site/downloads/hbql-0.9.0-alpha.zip hbql-0.9.0-alpha
+cd ..
+
+cd ./target/site
+zip -r ../../target/site/downloads/javadocs-0.9.0-alpha.zip apidocs
+cd ../..
+
+mvn site-deploy
