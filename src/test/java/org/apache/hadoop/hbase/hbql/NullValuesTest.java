7fdc64c799f36ca5aee28372fe199f8ad262db1c
Paul Ambrose <pambrose@mac.com>
2010-02-19 11:29:26 -0800

    Misc cleanup

--- src/test/java/org/apache/hadoop/hbase/hbql/NullValuesTest.java
+++ src/test/java/org/apache/hadoop/hbase/hbql/NullValuesTest.java
@@ -20,6 +20,7 @@
 
 package org.apache.hadoop.hbase.hbql;
 
+import org.apache.hadoop.hbase.hbql.client.HBatch;
 import org.apache.hadoop.hbase.hbql.client.HBqlException;
 import org.apache.hadoop.hbase.hbql.client.HConnection;
 import org.apache.hadoop.hbase.hbql.client.HConnectionManager;
@@ -36,52 +37,36 @@ import java.util.Random;
 
 public class NullValuesTest extends TestSupport {
 
-    static HConnection connection = null;
+    static HConnection conn = null;
 
     static Random randomVal = new Random();
 
     @BeforeClass
     public static void beforeClass() throws HBqlException {
 
-        connection = HConnectionManager.newConnection();
+        conn = HConnectionManager.newConnection();
 
-        connection.execute("CREATE TEMP MAPPING table30"
-                           + "("
-                           + "keyval key, "
-                           + "f1 ("
-                           + "  val1 string alias val1, "
-                           + "  val2 date alias val2, "
-                           + "  val3 int alias val3, "
-                           + "  val4 int[] alias val4, "
-                           + "  val5 object alias val5, "
-                           + "  val6 string alias val6 "
-                           + "))");
+        conn.execute("CREATE TEMP MAPPING table30"
+                     + "("
+                     + "keyval key, "
+                     + "f1 ("
+                     + "  val1 string alias val1, "
+                     + "  val2 date alias val2, "
+                     + "  val3 int alias val3, "
+                     + "  val4 int[] alias val4, "
+                     + "  val5 object alias val5, "
+                     + "  val6 string alias val6 "
+                     + "))");
 
-        if (!connection.tableExists("table30"))
-            System.out.println(connection.execute("create table table30 (f1())"));
+        if (!conn.tableExists("table30"))
+            System.out.println(conn.execute("create table table30 (f1())"));
         else
-            System.out.println(connection.execute("delete from table30"));
-
-        HPreparedStatement stmt = connection.prepareStatement(
-                "insert into table30 "
-                + "(keyval, val1, val2, val3, val4, val5) values "
-                + "(:key, :val1, :val2, :val3, :val4, :val5)");
-
-        final String keyval = Util.getZeroPaddedNonNegativeNumber(1, TestSupport.keywidth);
-
-        stmt.setParameter("key", keyval);
-        stmt.setParameter("val1", null);
-        stmt.setParameter("val2", new Date(System.currentTimeMillis()));
-        stmt.setParameter("val3", 0);
-        stmt.setParameter("val4", null);
-        stmt.setParameter("val5", null);
-
-        stmt.execute();
+            System.out.println(conn.execute("delete from table30"));
     }
 
     public void queryRecords(final int exepected, final String query) throws HBqlException {
 
-        HResultSet<HRecord> resultSet = connection.executeQuery(query);
+        HResultSet<HRecord> resultSet = conn.executeQuery(query);
 
         System.out.println("Query: " + query);
 
@@ -110,7 +95,59 @@ public class NullValuesTest extends TestSupport {
 
 
     @Test
-    public void simpleInsert() throws HBqlException {
+    public void insert1() throws HBqlException {
+
+        System.out.println(conn.execute("delete from table30"));
+
+        HPreparedStatement stmt = conn.prepareStatement(
+                "insert into table30 "
+                + "(keyval, val1, val2, val3, val4, val5) values "
+                + "(:keyval, :val1, :val2, :val3, :val4, :val5)");
+
+        final String keyval = Util.getZeroPaddedNonNegativeNumber(1, TestSupport.keywidth);
+
+        stmt.setParameter("keyval", keyval);
+        stmt.setParameter("val1", null);
+        stmt.setParameter("val2", new Date(System.currentTimeMillis()));
+        stmt.setParameter("val3", 0);
+        stmt.setParameter("val4", null);
+        stmt.setParameter("val5", null);
+
+        stmt.execute();
+        queryRecords(1, "SELECT * FROM table30");
+        queryRecords(1, "SELECT * FROM table30 with server filter where null is null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where null is not null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where val1 is not null");
+        queryRecords(1, "SELECT * FROM table30 with server filter where val1 is null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where val2 is null");
+        queryRecords(1, "SELECT * FROM table30 with server filter where val2 is not null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where val3 is null");
+        queryRecords(1, "SELECT * FROM table30 with server filter where val3 is not null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where val4 is not null");
+        queryRecords(1, "SELECT * FROM table30 with server filter where val4 is null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where val5 is not null");
+        queryRecords(1, "SELECT * FROM table30 with server filter where val5 is null");
+        queryRecords(0, "SELECT * FROM table30 with server filter where definedInRow(val6)");
+        queryRecords(1, "SELECT * FROM table30 with server filter where not definedInRow(val6)");
+    }
+
+    @Test
+    public void insert2() throws HBqlException {
+
+        System.out.println(conn.execute("delete from table30"));
+
+        final String keyval = Util.getZeroPaddedNonNegativeNumber(1, TestSupport.keywidth);
+        HRecord rec = conn.getMapping("table30").newHRecord();
+        rec.setCurrentValue("keyval", keyval);
+        rec.setCurrentValue("val1", null);
+        rec.setCurrentValue("val2", new Date(System.currentTimeMillis()));
+        rec.setCurrentValue("val3", 0);
+        rec.setCurrentValue("val4", null);
+        rec.setCurrentValue("val5", null);
+
+        final HBatch<HRecord> batch = conn.newHBatch();
+        batch.insert(rec);
+        batch.apply();
 
         queryRecords(1, "SELECT * FROM table30");
         queryRecords(1, "SELECT * FROM table30 with server filter where null is null");
