64b2c6d75798cf62ece335d5ab3a82f0e3ea6717
Paul Ambrose <pambrose@mac.com>
2010-03-31 09:41:26 -0700

    Bumped version to 0.9.25.  Cleaned up install instructions for index jars.

--- src/main/java/org/apache/hadoop/hbase/hbql/util/package-info.java
+++ src/main/java/org/apache/hadoop/hbase/hbql/util/package-info.java
@@ -18,8 +18,8 @@
  * limitations under the License.
  */
 
-@VersionAnnotation(version = "0.9.24-alpha",
-                   date = "02/18/10",
+@VersionAnnotation(version = "0.9.25-alpha",
+                   date = "03/31/10",
                    user = "pambrose",
                    revision = "",
                    url = "http://www.hbql.com") package org.apache.hadoop.hbase.hbql.util;
